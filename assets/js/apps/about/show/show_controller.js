776172e89088f6b17a20ea6f8c8a0faf8876b89d
Dmitriy Borowskiy <d.borowskij@gmail.com>
2016-10-19 13:37:54 +0300

    page about

---
+++ assets/js/apps/about/show/show_controller.js
@@ -0,0 +1,9 @@
+ContactManager.module("AboutApp.Show", function(Show, ContactManager,
+Backbone, Marionette, $, _){
+    Show.Controller = {
+        showAbout: function(){
+            var view = new Show.Message();
+            ContactManager.mainRegion.show(view);
+        }
+    };
+});
